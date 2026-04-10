7c7a430843bcf4bc9a3aeb0450bdd6322d328047
MomoCrash <44437470+MomoCrash@users.noreply.github.com>
2026-04-10 11:32:13 +0200

    FIX paths

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,9 @@ function App() {
               <Routes>
                   <Route path="/" element={<Layout/>}>
                       <Route index element={<Home />} />
-                      <Route path={"../credits"} element={<Credits />} />
-                      <Route path={"../games"} element={<Game />} />
-                      <Route path={"../*"} element={<NotFound />} />
+                      <Route path="credits" element={<Credits />} />
+                      <Route path="games" element={<Game />} />
+                      <Route path="*" element={<NotFound />} />
                   </Route>
               </Routes>
           </BrowserRouter>
@@ -34,3 +34,4 @@ function App() {
 }
 
 export default App
+
